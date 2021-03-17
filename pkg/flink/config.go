2ecfa34b34bf0528f8c7b613ea45ba851bf89f3e
Filipe Regadas <filiperegadas@gmail.com>
2021-03-17 07:04:47 +0000

    Allow setting JobManager AccessScope (#6)

--- pkg/flink/config.go
+++ pkg/flink/config.go
@@ -25,6 +25,7 @@ type JobManagerConfig struct {
 	Memory       resource.Quantity  `json:"memory" pflag:"amount of memory per pod"`
 	NodeSelector map[string]string  `json:"nodeSelector" pflag:"Annotates the JobManager resource with desired nodepool type"`
 	Sidecars     []corev1.Container `json:"sidecars" pflag:"Sidecar containers running alongside with the JobManager container in the pod"`
+	AccessScope  *string            `json:"accessScope" pflag:"Access scope of the JobManager service. Cluster (default), VPC, External, NodePort, Headless."`
 }
 
 type TaskManagerConfig struct {
