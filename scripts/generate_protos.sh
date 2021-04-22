35d0689a5c26b1626a28e5a553fbe7afd315c31d
Filipe Regadas <filiperegadas@gmail.com>
2021-04-22 09:59:02 +0100

    Only generate protobuf for go

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -6,11 +6,5 @@ LYFT_IMAGE="lyft/protocgenerator:8167e11d3b3439373c2f033080a4b550078884a2"
 
 docker run --rm -u $(id -u):$(id -g) -v $DIR:/defs $LYFT_IMAGE -i ./protos -d protos -l go --go_source_relative --validate_out
 
-languages=("python" "cpp" "java")
-for lang in "${languages[@]}"
-do
-    docker run --rm -u $(id -u):$(id -g) -v $DIR:/defs $LYFT_IMAGE -i ./protos -d protos -l $lang
-done
-
 # Docs generated
 docker run --rm -u $(id -u):$(id -g) -e REPO_BLOB_SHA=master -e PROJECT_ANNOTATION_PREFIX=flyte.interface -v $DIR:/defs $LYFT_IMAGE -i ./protos -d protos -l protodoc
