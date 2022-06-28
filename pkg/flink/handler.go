c2fe52c40e6e67575f93451f4f1c307475ba7b97
live-wire <dhruv.life@hotmail.com>
2022-06-28 12:00:08 +0200

    Removed logging from helper func

--- pkg/flink/handler.go
+++ pkg/flink/handler.go
@@ -239,7 +239,6 @@ func flinkClusterTaskInfo(ctx context.Context, flinkCluster *flinkOp.FlinkCluste
 }
 
 func isSubmitterExitCodeRetryable(ctx context.Context, exitCode int32) bool {
-	logger.Infof(ctx, "job submitter failed: %d", exitCode)
 	config := GetFlinkConfig()
 	for _, ec := range config.NonRetryableExitCodes {
 		if exitCode == ec {
