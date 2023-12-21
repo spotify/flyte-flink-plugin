012482c2adccdad5d04107e5369179db45ad303f
Filipe Regadas <oss@regadas.email>
2023-12-21 14:43:34 +0000

    Apply suggestions from code review

--- pkg/flink/handler.go
+++ pkg/flink/handler.go
@@ -97,7 +97,7 @@ func NewFlinkTaskContext(ctx context.Context, taskCtx FlinkTaskExecContext) (*Fl
 		Namespace:            taskMetadata.GetNamespace(),
 		Annotations:          GetDefaultAnnotations(taskMetadata),
 		Labels:               GetDefaultLabels(taskMetadata),
-		EnvironmentVariables: GetDefaultEnvVars(taskMetadata),
+		EnvironmentVariables: GetEnvironmentVariables(taskMetadata),
 		Job:                  job,
 	}, nil
 }
